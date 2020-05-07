4727ee6813f82252c5db582ac3b6fc94b1f85413
MurderousCrotch <1132464757@qq.com>
2020-05-07 21:59:49 +0900

    [Matt]第二个界面初步

--- Assets/Scripts/1-3_Move/MenuPanelControl.cs
+++ Assets/Scripts/1-3_Move/MenuPanelControl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MenuPanelControl : MonoBehaviour
 {
     public GameObject rolePanel;
+    public GameObject equipPanel;
 
     // Start is called before the first frame update
     void Start()
@@ -14,12 +15,28 @@ public class MenuPanelControl : MonoBehaviour
 
     public void Button_1()
     {
-        rolePanel.SetActive(true);
+        if (!rolePanel.activeSelf)
+        {
+            rolePanel.SetActive(true);
+            equipPanel.SetActive(false);
+        }
+        else
+        {
+            rolePanel.SetActive(false);
+        }
     }
 
     public void Button_2()
     {
-
+        if (!equipPanel.activeSelf)
+        {
+            equipPanel.SetActive(true);
+            rolePanel.SetActive(false);
+        }
+        else
+        {
+            equipPanel.SetActive(false);
+        }
     }
 
     public void Button_3()
