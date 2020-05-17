9f6fb5db4468a97dd8a6aaeb6c63c5e4cbe0f136
MattJasonLu <1132464757@qq.com>
2020-05-17 12:20:26 +0900

    [Matt]鼠标悬浮提示

---
+++ Assets/Scripts/1-3_Move/ShowHoverHint.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShowHoverHint : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    GameObject hint;
+    bool isShowHint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hint = transform.Find("Desp").gameObject;
+        hint.SetActive(false);
+        isShowHint = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isShowHint = true;
+        hint.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isShowHint = false;
+        hint.SetActive(false);
+    }
+}
