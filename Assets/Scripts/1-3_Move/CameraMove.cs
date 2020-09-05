d155a158278c24565e8cf78f65ba9c41fc29a4ee
MattJasonLu <1132464757@qq.com>
2020-09-05 12:03:50 +0800

    [Matt]调整场景

--- Assets/Scripts/1-3_Move/CameraMove.cs
+++ Assets/Scripts/1-3_Move/CameraMove.cs
@@ -21,6 +21,10 @@ public class CameraMove : MonoBehaviour {
 	Vector3 m_RoleOriginPos;
 	Vector3 m_RoleOriginRot;
 	Vector3 m_EnemyOriginPos;
+	/// <summary>
+	/// 标识是否移动到位
+	/// </summary>
+	private bool m_isOver;
 
 	void Awake()
 	{
@@ -38,15 +42,7 @@ public class CameraMove : MonoBehaviour {
 		if (m_State == GameState.Move)
 		{
 			transform.position = player.transform.position + offset;
-			if (Input.GetKeyDown (KeyCode.F1))
-			{
-				m_OriginPos = transform.position;
-				m_OriginRot = transform.localEulerAngles;
-				m_RoleOriginPos = m_Role.transform.position;
-				m_RoleOriginRot = m_Role.transform.Find("Anim").localEulerAngles;
-				m_EnemyOriginPos = m_Enemy.transform.position;
-				m_State = GameState.Battle;
-			}
+			
 		}
 		else if (m_State == GameState.Battle)
 		{
@@ -60,6 +56,20 @@ public class CameraMove : MonoBehaviour {
 	}
 
 	/// <summary>
+    /// 进入战斗
+    /// </summary>
+    public void OnClickIntoBattle()
+    {
+		m_OriginPos = transform.position;
+		m_OriginRot = transform.localEulerAngles;
+		m_RoleOriginPos = m_Role.transform.position;
+		m_RoleOriginRot = m_Role.transform.Find("Anim").localEulerAngles;
+		m_EnemyOriginPos = m_Enemy.transform.position;
+		m_State = GameState.Battle;
+		m_isOver = false;
+    }
+
+	/// <summary>
 	/// 更改战斗视角
 	/// </summary>
 	public void ChangeBattleView()
@@ -69,36 +79,42 @@ public class CameraMove : MonoBehaviour {
 
 	IEnumerator Change()
 	{
-		// 人物变更战斗状态
-		m_Role.transform.Find("Anim").GetComponentInChildren<Animator>().SetTrigger("Battle");
+		if (!m_isOver)
+		{
+			// 人物变更战斗状态
+			m_Role.transform.Find("Anim").GetComponentInChildren<Animator>().SetTrigger("Battle");
 
-		// 镜头向前移动，人物后退
-		if (Mathf.Abs(transform.position.z - m_OriginPos.z) < 0.7f)
-		{
-			Vector3 newPos = new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.7f);
-			transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * 2);
+			// 镜头向前移动
+			if (Mathf.Abs(transform.position.z - m_OriginPos.z) < 0.5f)
+			{
+				Vector3 newPos = new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.5f);
+				transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * 2);
+			}
+			// 人物后退
+			if (Mathf.Abs(m_Role.transform.position.x - m_RoleOriginPos.x) < 1.5f)
+			{
+				Vector3 newPos = new Vector3(m_Role.transform.position.x - 1.5f, m_Role.transform.position.y, m_Role.transform.position.z);
+				m_Role.transform.position = Vector3.Lerp(m_Role.transform.position, newPos, Time.deltaTime * 2);
+			}
+			// 敌人入场
+			if (Mathf.Abs(m_Enemy.transform.position.y - m_EnemyOriginPos.y) < 3)
+			{
+				Vector3 newPos = new Vector3(m_Enemy.transform.position.x, m_Enemy.transform.position.y - 3, m_Enemy.transform.position.z);
+				m_Enemy.transform.position = Vector3.Lerp(m_Enemy.transform.position, newPos, Time.deltaTime * 2);
+			}
+			
+			// 镜头和人物绕着X轴旋转
+			if (Mathf.Abs(transform.localEulerAngles.x - m_OriginRot.x) < 20)
+			{
+				transform.Rotate(Vector3.left * Time.deltaTime * 40);
+			}
+			if (Mathf.Abs(m_Role.transform.Find("Anim").localEulerAngles.x - m_RoleOriginRot.x) < 20)
+			{
+				m_Role.transform.Find("Anim").Rotate(Vector3.left * Time.deltaTime * 40);
+			}
+			yield return new WaitForSeconds(1f);
+			m_isOver = true;
 		}
-		if (Mathf.Abs(m_Role.transform.position.x - m_RoleOriginPos.x) < 1.5f)
-		{
-			Vector3 newPos = new Vector3(m_Role.transform.position.x - 1.5f, m_Role.transform.position.y, m_Role.transform.position.z);
-			m_Role.transform.position = Vector3.Lerp(m_Role.transform.position, newPos, Time.deltaTime * 2);
-		}
-		if (Mathf.Abs(m_Enemy.transform.position.y - m_EnemyOriginPos.y) < 3)
-		{
-			Vector3 newPos = new Vector3(m_Enemy.transform.position.x, m_Enemy.transform.position.y - 3, m_Enemy.transform.position.z);
-			m_Enemy.transform.position = Vector3.Lerp(m_Enemy.transform.position, newPos, Time.deltaTime * 2);
-		}
-		//yield return new WaitForSeconds(0.3f);
-		// 镜头和人物绕着X轴旋转
-		if (Mathf.Abs(transform.localEulerAngles.x - m_OriginRot.x) < 20)
-		{
-			transform.Rotate(Vector3.left * Time.deltaTime * 40);
-		}
-		if (Mathf.Abs(m_Role.transform.Find("Anim").localEulerAngles.x - m_RoleOriginRot.x) < 20)
-		{
-			m_Role.transform.Find("Anim").Rotate(Vector3.left * Time.deltaTime * 40);
-		}
-		yield return new WaitForSeconds(0.3f);
 	}
 
 	/// <summary>
--- Assets/Scripts/2-1_Game/Menu.cs
+++ Assets/Scripts/2-1_Game/Menu.cs
@@ -11,12 +11,11 @@ public class Menu : MonoBehaviour
     [SerializeField]
     private GameObject m_Effect_2;
 
-    /// <summary>
-    /// 进入战斗
-    /// </summary>
-    public void OnClickIntoBattle()
+    private bool isMove = false;
+
+    void Update() 
     {
-        Debug.Log("Into Battle");
+        MoveTowards();
     }
 
     /// <summary>
@@ -24,15 +23,45 @@ public class Menu : MonoBehaviour
     /// </summary>
     public void OnClickSkill_1()
     {
-        Debug.Log("Skill_1");
         StartCoroutine(PlaySkill1());
     }
 
+    public void OnClickSkill_2()
+    {
+        StartCoroutine(PlaySkill2());
+    }
+
+    /// <summary>
+    /// 人物向前移动
+    /// </summary>
+    public void MoveTowards()
+    {
+        if (isMove)
+        {
+            // 主角人物向前移动
+            if (m_Player.transform.position.x < -1f)
+            {
+                Vector3 newPos = new Vector3(m_Player.transform.position.x + 1.5f, m_Player.transform.position.y, m_Player.transform.position.z);
+                m_Player.transform.position = Vector3.Lerp(m_Player.transform.position, newPos, Time.deltaTime * 2);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 播放技能1
+    /// </summary>
+    /// <returns></returns>
     IEnumerator PlaySkill1()
     {
+        isMove = true;
+        yield return new WaitForSeconds(0.3f);
+        isMove = false;
+        // 播放人物攻击动画
         m_Player.transform.Find("Anim").GetComponent<Animator>().ResetTrigger("Battle");
         m_Player.transform.Find("Anim").GetComponent<Animator>().SetTrigger("Skill1");
+        // 延时
         yield return new WaitForSeconds(0.3f);
+        // 播放效果
         GameObject effect = Instantiate(m_Effect_1);
         effect.transform.SetParent(m_Player.transform, false);
         effect.transform.position = new Vector3(
@@ -41,19 +70,30 @@ public class Menu : MonoBehaviour
         Destroy(effect, 2f);
         m_Player.transform.Find("Anim").GetComponent<Animator>().ResetTrigger("Skill1");
         m_Player.transform.Find("Anim").GetComponent<Animator>().SetTrigger("Battle");
-        
     }
 
     /// <summary>
-    /// 测试技能2
+    /// 播放技能2
     /// </summary>
-    public void OnClickSkill_2()
+    /// <returns></returns>
+    IEnumerator PlaySkill2()
     {
-        Debug.Log("Skill_2");
+        isMove = true;
+        yield return new WaitForSeconds(0.3f);
+        isMove = false;
+        // 播放人物攻击动画
+        m_Player.transform.Find("Anim").GetComponent<Animator>().ResetTrigger("Battle");
+        m_Player.transform.Find("Anim").GetComponent<Animator>().SetTrigger("Skill1");
+        // 延时
+        yield return new WaitForSeconds(0.3f);
+        // 播放效果
         GameObject effect = Instantiate(m_Effect_2);
         effect.transform.SetParent(m_Player.transform, false);
-        effect.transform.localEulerAngles = new Vector3(0 , 90, 0);
+        effect.transform.position = new Vector3(
+            effect.transform.position.x + 0.3f, effect.transform.position.y + 0.2f, effect.transform.position.z);
         effect.GetComponent<ParticleSystem>().Play();
         Destroy(effect, 2f);
+        m_Player.transform.Find("Anim").GetComponent<Animator>().ResetTrigger("Skill1");
+        m_Player.transform.Find("Anim").GetComponent<Animator>().SetTrigger("Battle");
     }
 }
